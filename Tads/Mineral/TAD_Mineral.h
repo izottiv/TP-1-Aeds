f878d8e13dbde6993261e260081984c8191f5922
Gabriel <gvitor922@gmail.com>
2024-11-07 11:15:25 -0300

    Commit verdadeiro

--- Tads/Mineral/TAD_Mineral.h
+++ Tads/Mineral/TAD_Mineral.h
@@ -1,12 +1,26 @@
 #ifndef TAD_MINERAL_
 #define TAD_MINERAL_
 
+//Definindo as Cores
+typedef enum{
+    Acinzentado,
+    Amarelo,
+    Azulado,
+    Marrom,
+    Vermelho
+}Cores;
+
 //Criando o tipo Mineral
 typedef struct{
     char nome[10];
     float dureza;
     float reatividade;
-};
+    Cores Cor;
+}Mineral;
+
+//Sets
+void setNome(Mineral* amostra, char* nome);
+void setDureza(Mineral* amostra, float dureza);
 
 
 
