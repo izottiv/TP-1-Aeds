3673a6e7e472801527b974053a42c24dabda49be
izottiv <vitorafonso20040@gmail.com>
2024-11-07 11:29:10 -0300

    finalizando o .h

--- Tads/Mineral/TAD_Mineral.h
+++ Tads/Mineral/TAD_Mineral.h
@@ -1,5 +1,5 @@
-#ifndef TAD_MINERAL_
-#define TAD_MINERAL_
+#ifndef TAD_MINERAL_H
+#define TAD_MINERAL_H
 
 //Definindo as Cores
 typedef enum{
@@ -21,8 +21,16 @@ typedef struct{
 //Sets
 void setNome(Mineral* amostra, char* nome);
 void setDureza(Mineral* amostra, float dureza);
+void setReatividade(Mineral* amostra, float reatividade);
+void setCor(Mineral* amostra, Cores cor);
 
+//Gets
+char* getNome(Mineral* amostra);
+float getDureza(Mineral* amostra);
+float getReatividade(Mineral* amostra);
+Cores getCor(Mineral* amostra);
 
-
+//inicialização
+void inicializacao(Mineral* amostra, char* nome, float dureza, float reatividade, Cores cor);
 
 #endif
