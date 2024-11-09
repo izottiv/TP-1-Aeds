50ae3d23b78e2cbfaf7e081d234cb9e058d8cb80
Vitor Carvalho <vitorafonso20040@gmail.com>
2024-11-09 19:29:53 -0300

    finalizando tad 1, acredito eu

--- Tads/Mineral/TAD_Mineral.c
+++ Tads/Mineral/TAD_Mineral.c
@@ -31,9 +31,61 @@ Cores getCor(Mineral* amostra){
 }
 
 //Definindo a inicialização
-void inicializacao(Mineral* amostra, char* nome, float dureza, float reatividade, Cores cor){
-    setNome(amostra, nome);
-    setDureza(amostra, dureza);
-    setReatividade(amostra, reatividade);
-    setCor(amostra, cor);
+void Inicializacao(Mineral* amostra, char* mineral){
+    if(strcmp(mineral, "Ferrolita") == 0){
+        setNome(amostra, "Ferrolita");
+        setDureza(amostra, 0.5);
+        setReatividade(amostra, 0.7);
+        setCor(amostra, 0);
+    }
+    if(strcmp(mineral, "Solarium") == 0){
+        setNome(amostra, "Solarium");
+        setDureza(amostra, 0.9);
+        setReatividade(amostra, 0.2);
+        setCor(amostra, 1);
+    }
+    if(strcmp(mineral, "Aquavitae") == 0){
+        setNome(amostra, "Aquavitae");
+        setDureza(amostra, 0.5);
+        setReatividade(amostra, 0.8);
+        setCor(amostra, 2);
+    }
+    if(strcmp(mineral, "Terranita") == 0){
+        setNome(amostra, "Terranita");
+        setDureza(amostra, 0.7);
+        setReatividade(amostra, 0.6);
+        setCor(amostra, 3);
+    }
+    if(strcmp(mineral, "Calaris") == 0){
+        setNome(amostra, "Calaris");
+        setDureza(amostra, 0.6);
+        setReatividade(amostra, 0.5);
+        setCor(amostra, 4);
+    }
+}
+
+void imprimir(Mineral* amostra){
+    char tipo[50];
+    switch (amostra->Cor)
+    {
+    case Acinzentado:
+        strcpy(tipo, "Acinzentado");
+        break;
+    case Amarelo:
+        strcpy(tipo, "Amarelo");
+        break;
+    case Azulado:
+        strcpy(tipo, "Azulado");
+        break;
+    case Marrom:
+        strcpy(tipo, "Marrom");
+        break;
+    case Vermelho:
+        strcpy(tipo, "Vermelho");
+        break;
+    default:
+        break;
+    }
+    printf("Nome: %s\nDureza: %f\nReatividade: %f\nCor: %s", getNome(amostra), 
+    getDureza(amostra), getReatividade(amostra), tipo);
 }
