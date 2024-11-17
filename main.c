6afb92d50c7128e2b63c29884ab1e4270ad5ba20
xXpedrigoXx <xxxpedrigoxxx@gmail.com>
2024-11-16 22:26:51 -0300

    preprint

--- main.c
+++ main.c
@@ -3,13 +3,25 @@
 #include "src/ListaSonda/TAD_ListaSonda.h"
 
 int main(){
-<<<<<<< HEAD
+
+    Localizacao loc;
+    loc.Latitude = 7777;
+    loc.Longitude = 8888;
+    ListaMineral mineralp;
+    RochaMineral Batata,cenoura,beterraba,rabanete;
+    GerenciadorCompartimento pedro;
+    InicializaRochaMineral(&Batata,10,mineralp,Ferrom,loc);
+    InicializaRochaMineral(&cenoura,15,mineralp,Terrasol,loc);
+    InicializaRochaMineral(&rabanete,20,mineralp,Calquer,loc);
+    InicializadorCompartimento(&pedro);
+    InserirRocha(&pedro,&Batata,500);
+    InserirRocha(&pedro,&cenoura,500);
+    InserirRocha(&pedro,&rabanete,500);
+    ImprimeConteudoCompartimento(&pedro);
+    printf("---------------------------------------------\n");
+    RemoverRochaPorCategoria(&pedro,&beterraba,Ferrom);
+    RemoverRochaPorCategoria(&pedro,&beterraba,Terrasol);
+
+    ImprimeConteudoCompartimento(&pedro);
     return 0;
-=======
-
-ListaSondas sonda;
-InicializaListaSondas(&sonda);
-
-return 0;
->>>>>>> vitor
 }
--- src/ListaSonda/SondaEspacial/Compartimento/TAD_Compartimento.c
+++ src/ListaSonda/SondaEspacial/Compartimento/TAD_Compartimento.c
@@ -38,8 +38,10 @@ void ImprimeConteudoCompartimento(GerenciadorCompartimento *comp){
             printf("=================================\n");
             printf("Indentificador: %d\n",aux->_RochaMineral.Identificador);
             printf("Peso: %.2f\n",aux->_RochaMineral.Peso);
-            printf("Minerais: \n"); //TERMINAR DEPOIS
-            printf("Categoria: %d\n",aux->_RochaMineral._Categorias); // TERMINAR DEPOIS
+            printf("Minerais:\n");
+            printf("Categoria: ");
+            TransformarCategoria(&aux->_RochaMineral);
+            printf("\n");
             printf("Latitude %.6f Longitude: %.6f\n",aux->_RochaMineral._Localizacao.Latitude,aux->_RochaMineral._Localizacao.Longitude);
             printf("Data De Coleta:\n");
             printf("%d/%d/%d as %d:%d:%d\n",aux->_RochaMineral._DataColeta.dia, aux->_RochaMineral._DataColeta.mes, aux->_RochaMineral._DataColeta.ano, aux->_RochaMineral._DataColeta.hora, aux->_RochaMineral._DataColeta.minuto, aux->_RochaMineral._DataColeta.segundo);
@@ -133,8 +135,8 @@ void TrocaRocha(GerenciadorCompartimento *comp, RochaMineral *Rocha){
 
 void RemoverRochaPorCategoria(GerenciadorCompartimento*comp, RochaMineral *RochaRetirada, Categorias Categoria){
     Compartimento *aux, *AuxCont;
-    if (VerificaSeVazia(comp) == 0){
-        printf("lista nao ta vazia pai\n");
+    if (VerificaSeVazia(comp) == 1){
+        printf("LISTA VAZIA\n");
         return;
     }
     int possicaosecundaria;
@@ -147,38 +149,10 @@ void RemoverRochaPorCategoria(GerenciadorCompartimento*comp, RochaMineral *Rocha
                 aux = comp->PrimeiroRocha;
                 comp->PrimeiroRocha = comp->PrimeiroRocha->Prox;
                 free(aux);
-                break;
-                }
-            }
-            else if(pos == RetornaTamanho(comp)){   // No caso do ultimo elemento da lista ser da mesma categoria
-                pos = 1;
-                AuxCont = comp->PrimeiroRocha->Prox;
-                while (AuxCont->Prox != NULL){
-                    if (pos == RetornaTamanho(comp)-1){
-                        aux = AuxCont->Prox;
-                        AuxCont->Prox == NULL;
-                        free(aux);
-                        break;
-                    }
-                    pos++;
-                }
+                printf("teste inicio\n");
                 break;
             }
-            else{ // Se o elemento da lista ser da mesma categoria estja no meio
-                aux = AuxCont;
-                possicaosecundaria = pos;
-                pos = 0;
-                AuxCont = comp->PrimeiroRocha->Prox;
-                while (AuxCont->Prox != NULL){
-                    if(pos == possicaosecundaria-1){
-                        AuxCont->Prox = aux->Prox;
-                        free(aux);
-                        break;
-                    }
-                    pos++;
-                    AuxCont = AuxCont->Prox;
-                }
-            }
+        }
         pos++;
         AuxCont = AuxCont->Prox;
     }
